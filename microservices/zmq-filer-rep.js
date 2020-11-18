e5c07da575942365dc1a1f4abec9fe2604c41830
Tim Austin <tim@neenjaw.com>
2020-11-18 07:58:30 -0600

    microservices

--- microservices/zmq-filer-rep.js
+++ microservices/zmq-filer-rep.js
@@ -10,6 +10,21 @@ responder.on('message', (data) => {
   console.log(`Received request to get: ${request.path}`)
 
   fs.readFile(request.path, (err, content) => {
+    if (err) {
+      responder.send(
+        JSON.stringify({
+          error: [
+            {
+              type: 'read-error',
+              msg: `Unable to get: ${request.path}`,
+            },
+          ],
+        })
+      )
+
+      return
+    }
+
     console.log(`Sending response content.`)
     responder.send(
       JSON.stringify({
@@ -21,11 +36,23 @@ responder.on('message', (data) => {
   })
 })
 
-responder.bind('tcp://127.0.0.1:60401', (err) => {
+responder.bindSync('tcp://127.0.0.1:60401', (err) => {
   console.log('Listening for zmq requesters...')
 })
 
 process.on('SIGINT', () => {
   console.log('Shutting down...')
+  responder.close()
+})
+
+process.on('SIGTERM', () => {
+  console.log('Shutting down...')
+  responder.close()
+})
+
+// To catch errors, need to bindSync the port for the zmq socket
+// Otherwise it errors, "socket is busy"
+process.on('uncaughtException', () => {
+  console.log('Error encountered. Shutting down...')
   responder.close()
 })
